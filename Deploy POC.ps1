d960cdb43199fd1fb855d0a8d170de23c7c20957
ans-cst <Cloudservices.Servicedesk@ansgroup.co.uk>
2018-06-19 16:00:20 +0100

    Latest POC Deployment 19/06/2018

--- Deploy POC.ps1
+++ Deploy POC.ps1
@@ -1,27 +1,50 @@
-﻿#Deploy Imperial POC
+﻿# Deploy Imperial POC
 
+#Install and Import AzureRM Module
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Importing module..."
+Import-Module -Name AzureRM -ErrorVariable ModuleError -ErrorAction SilentlyContinue
+If ($ModuleError) {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Installing module..."
+    Install-Module -Name AzureRM
+    Import-Module -Name AzureRM
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Installed module..."
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Imported module"
+Write-Host ""
+
+#Login to Azure
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Logging in to Azure Account..."
 Login-AzureRmAccount
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully logged in to Azure Account"
+Write-Host ""
 
-$SubID = Read-Host "Enter your Subscription Id"
+#Select SubscriptionId
+$SubId = Read-Host "Please input your Subscription Id"
+while ($SubId.Length -le 35) {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Subscription Id not valid"
+    $SubId = Read-Host "Please input your Subscription Id"
+}
+Select-AzureRmSubscription -SubscriptionId $SubId
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Subscription successfully selected"
+Write-Host ""
 
-Select-AzureRmSubscription -SubscriptionId 9c15dde4-f4fa-4de2-8655-8b81421fd628
-
+#Configure Azure Location to Deploy POC
 $Location = Read-Host "Enter the location to deploy the POC"
 
-#Create Required Resource Groups
+# Create Required Resource Groups
 New-AzureRmResourceGroup -Name iac-uks-network-poc-rg -Location "West Europe"
 New-AzureRmResourceGroup -Name iac-uks-desktop-poc-rg -Location "West Europe"
 New-AzureRmResourceGroup -Name iac-uks-function-poc-rg -Location "West Europe"
 
 
-#Deploy Core Networking Resources
+# Deploy Core Networking Resources
 New-AzureRmResourceGroupDeployment -ResourceGroupName iac-uks-network-poc-rg -TemplateUri https://raw.githubusercontent.com/ans-rfroggatt/Imperial-POC/master/Networking-Master.json `
 -TemplateParameterUri https://raw.githubusercontent.com/ans-rfroggatt/Imperial-POC/master/Networking-Master-Parameters.json
 
 
-#Deploy Azure Function App
-New-AzureRmResourceGroupDeployment -ResourceGroupName iac-uks-function-poc-rg -TemplateUri
+# Deploy Azure Function App
+##New-AzureRmResourceGroupDeployment -ResourceGroupName iac-uks-function-poc-rg -TemplateUri
 
 
-#Trigger Azure Function to Deploy Test Desktop
-.\Deploy-Windows-VM.ps1
+# Trigger Azure Function to Deploy Test Desktop
+##.\Deploy-Windows-VM.ps1
